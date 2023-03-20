525e8c0c3fbb031d0f697dcc172fea6e9123eb97
Shraddha Umbarkar <shraddhaumbarkar23@gmail.com>
2023-03-20 20:49:50 +0530

    first commit

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:139485546018:web:94653447d17edfc77dac7b";
+  static String apiKey = "AIzaSyDshuioyeRxE8sdaud45H_86kE2JbWMMhQ";
+  static String messagingSenderId = "139485546018";
+  static String projectId = "chatappflutter-85ddf";
+  final primaryColor = const Color(0xffffffff);
+}
